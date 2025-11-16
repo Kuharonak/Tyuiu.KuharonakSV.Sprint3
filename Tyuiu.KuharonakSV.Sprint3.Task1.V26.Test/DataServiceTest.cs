33fc361eec01ed8086a683945526a1b32b5a08e2
СЕГА <skuhronak@gmail.com>
2025-11-16 14:10:30 +0300

    Task1

---
+++ Tyuiu.KuharonakSV.Sprint3.Task1.V26.Test/DataServiceTest.cs
@@ -0,0 +1,11 @@
+﻿namespace Tyuiu.KuharonakSV.Sprint3.Task1.V26.Test
+{
+    [TestClass]
+    public sealed class Test1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
