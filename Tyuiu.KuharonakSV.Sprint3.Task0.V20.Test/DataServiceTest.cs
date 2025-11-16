04eb32e76fc2d0182e25075413f5818eae9448d7
СЕГА <skuhronak@gmail.com>
2025-11-16 13:11:32 +0300

    Task0

---
+++ Tyuiu.KuharonakSV.Sprint3.Task0.V20.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KuharonakSV.Sprint3.Task0.V20.Lib;
+
+namespace Tyuiu.KuharonakSV.Sprint3.Task0.V20.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void GetMultiplySeries()
+        {
+            DataService ds = new DataService();
+            int value = 1;
+            int startValue = 1;
+            int stopValue = 14;
+            double res = ds.GetMultiplySeries(value, startValue, stopValue);
+            double wait = 287.18;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KuharonakSV.Sprint3.Task0.V20/Program.cs
@@ -0,0 +1,35 @@
+﻿using Tyuiu.KuharonakSV.Sprint3.Task0.V20.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.Title = "Спринт #2| Выполнил:  Кухаронак С В| СМАРТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Базовые навыки работы в С#                                        *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #20                                                             *");
+        Console.WriteLine("* Выполнил: |   Кухаронак С В                                           *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая запрашивает у пользователя исходные данные, *");
+        Console.WriteLine("* вычисляет, и печатает результат на экране                               *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Исходные данные                                                         *");
+        Console.WriteLine("***************************************************************************");
+        int value = 1;
+        int startValue = 1;
+        int stopValue = 14;
+        Console.WriteLine("Переменная x = " + value);
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+        Console.WriteLine("********************************************************************************");
+        Console.WriteLine("* Результат:                                                                   *");
+        Console.WriteLine("********************************************************************************");
+        Console.WriteLine("Произведение ряда = " + ds.GetMultiplySeries(value, startValue, stopValue));
+        Console.ReadKey();
+    }
+}
