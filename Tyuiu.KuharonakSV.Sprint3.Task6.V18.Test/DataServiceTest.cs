4b46d43aa9cc907bb145f5ea9450a4a24893a5e5
СЕГА <skuhronak@gmail.com>
2025-11-23 22:34:27 +0300

    Task6

---
+++ Tyuiu.KuharonakSV.Sprint3.Task6.V18.Test/DataServiceTest.cs
@@ -0,0 +1,20 @@
+﻿using Tyuiu.KuharonakSV.Sprint3.Task6.V18.Lib;
+namespace Tyuiu.KuharonakSV.Sprint3.Task6.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = 9;
+            int stopValue = 18;
+
+            int res = ds.GetSumTheDivisors(startValue, stopValue);
+            int wait = 95;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KuharonakSV.Sprint3.Task6.V18/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.KuharonakSV.Sprint3.Task6.V18.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+
+        Console.WriteLine("*Напишите программу, которая ищет среди целых чисел, принадлежащих числово*");
+        Console.WriteLine("*ому отрезку [9, 18] сумму всех делителей меньше 10                       *");
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        int startValue = 9;
+        int stopValue = 18;
+
+        Console.WriteLine("Начало отрезка = " + startValue);
+        Console.WriteLine("Конец отрезка = " + stopValue);
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        
+
+        var result = ds.GetSumTheDivisors(startValue, stopValue);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
