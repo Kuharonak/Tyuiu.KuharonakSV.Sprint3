dc0a5817a172065d126e4af561a1643f8d14330a
СЕГА <skuhronak@gmail.com>
2025-11-23 22:42:53 +0300

    Task7

---
+++ Tyuiu.KuharonakSV.Sprint3.Task7.V5/Program.cs
@@ -0,0 +1,46 @@
+﻿using Tyuiu.KuharonakSV.Sprint3.Task7.V5.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.Title = "Спринт #3 | Выполнил: Кухаронак С В | СМАРТБ-25-1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #3                                                               *");
+        Console.WriteLine("* Тема: Вложенные циклы                                                   *");
+        Console.WriteLine("* Задание #7                                                              *");
+        Console.WriteLine("* Вариант #5                                                              *");
+        Console.WriteLine("* Выполнил: Кухаронак Сергей Витальевич | СМАРТБ-25-1                       *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("* Написать программу, которая выводит таблицу значений функции            *");
+        Console.WriteLine("* ((2 * Sin(x)) / (3 * x + 1.2)) + Cos(x) - 7 * x * 2                     *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        DataService ds  = new DataService();
+        int startValue = -5;
+        int stopValue = 5;
+        Console.WriteLine("Старт шага = " + startValue);
+        Console.WriteLine("Конец шага = " + stopValue);
+
+        double[] valueArray = new double[stopValue - startValue + 1];
+        valueArray = ds.GetMassFunction(startValue, stopValue);
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("+----------+----------+");
+        Console.WriteLine("|    X     |    f(x)  |");
+        Console.WriteLine("+----------+----------+");
+        for (int i = 0; i <= valueArray.Length - 1; i++)
+        {
+            Console.WriteLine("|{0,5:d}     |  {1, 6:f2}  |", startValue, valueArray[i]);
+            startValue++;
+        }
+        Console.WriteLine("+----------+----------+");
+        Console.ReadKey();
+    }
+}
