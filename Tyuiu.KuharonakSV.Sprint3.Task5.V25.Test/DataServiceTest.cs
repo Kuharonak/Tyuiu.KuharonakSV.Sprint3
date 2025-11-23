74612eaf87d1f7bdedce7ff3b21d21263f744e77
СЕГА <skuhronak@gmail.com>
2025-11-23 22:16:01 +0300

    Task5

---
+++ Tyuiu.KuharonakSV.Sprint3.Task5.V25.Test/DataServiceTest.cs
@@ -0,0 +1,25 @@
+﻿using Tyuiu.KuharonakSV.Sprint3.Task5.V25.Lib;
+namespace Tyuiu.KuharonakSV.Sprint3.Task5.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue1 = 1;
+            int stopValue1 = 3;
+
+            int startValue2 = 1;
+            int stopValue2 = 10;
+
+            int x = 2;
+
+            double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+            double wait = 7.621;
+
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.KuharonakSV.Sprint3.Task5.V25/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.KuharonakSV.Sprint3.Task5.V25.Lib;
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        Console.WriteLine("* УСЛОВИЕ:                                                                          *");
+        Console.WriteLine("* Написать программу, используя вложенные циклы, которая вычисляет сумму ряда       *");
+        Console.WriteLine("* по формуле при x = 2                                                              *");
+        Console.WriteLine("*                                                                                   *");
+        Console.WriteLine("*************************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                                  *");
+        Console.WriteLine("*************************************************************************************");
+
+        int startValue1 = 1;
+        int stopValue1 = 3;
+        int startValue2 = 1;
+        int stopValue2 = 10;
+        int x = 2;
+        double res = ds.GetSumSumSeries(x, startValue1, startValue2, stopValue1, stopValue2);
+        Console.WriteLine("Переменная X = " + x);
+        Console.WriteLine("Старт шага первой суммы ряда " + startValue1);
+        Console.WriteLine("Стоп шага первой суммы ряда " + stopValue1);
+        Console.WriteLine("Старт шага второй суммы ряда " + startValue2);
+        Console.WriteLine("Стоп шага второй суммы ряда " + stopValue2);
+
+        Console.WriteLine("*************************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                                        *");
+        Console.WriteLine("*************************************************************************************");
+
+        Console.WriteLine("Сумма ряда = " + res);
+        Console.ReadKey();
+    }
+}
