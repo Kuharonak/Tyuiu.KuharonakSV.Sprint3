955d7160c0f0190b1089d0e2c913204b2d8488b2
СЕГА <skuhronak@gmail.com>
2025-11-23 21:55:04 +0300

    Task4

--- Tyuiu.KuharonakSV.Sprint3.Task4.V25.Test/DataServiceTest.cs
+++ Tyuiu.KuharonakSV.Sprint3.Task4.V25.Test/DataServiceTest.cs
@@ -1,13 +1,14 @@
 ﻿using Tyuiu.KuharonakSV.Sprint3.Task4.V25.Lib;
 namespace Tyuiu.KuharonakSV.Sprint3.Task4.V25.Test
+{ 
 public class Tests
 {
-    [SetUp]
+    
     public void Setup()
     {
     }
 
-    [Test]
+    
     public void Test1()
     {
         DataService ds = new DataService();
