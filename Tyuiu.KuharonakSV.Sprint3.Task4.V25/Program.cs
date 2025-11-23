1e15a48598daf64bb382cc685695fe1bebc31677
СЕГА <skuhronak@gmail.com>
2025-11-23 21:52:17 +0300

    Task4

--- Tyuiu.KuharonakSV.Sprint3.Task4.V25/Program.cs
+++ Tyuiu.KuharonakSV.Sprint3.Task4.V25/Program.cs
@@ -13,27 +13,21 @@ internal class Program
         Console.WriteLine("* Задание #4                                                              *");
         Console.WriteLine("* Вариант #25                                                             *");
         Console.WriteLine("* Выполнил: Кухаронак С В | СМАРТБ-25-1                                   *");
-        Console.WriteLine("***************************************************************************");
         Console.WriteLine("* УСЛОВИЕ:                                                                *");
         Console.WriteLine("* На отрезке, где x принимает значения от -5 до 5, вычислить значение     *");
-        Console.WriteLine("* функции y=x/(cos(x)-sin(x)). При х = 0 пропустить значение. Полученные  *");
+        Console.WriteLine("* функции y=x/(cos(x)-sin(x)) При х = 0 пропустить значение. Полученные   *");
         Console.WriteLine("* значения суммировать.                                                   *");
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
         Console.WriteLine("***************************************************************************");
-
-        int startValue = -5;
-        int stopValue = 5;
-
-        Console.WriteLine(" Старт шага = " + startValue);
-        Console.WriteLine(" Конец шага = " + stopValue);
-
+        Console.WriteLine("Введите x: ");
+        int start = Convert.ToInt32(Console.ReadLine());
+        Console.WriteLine("Введите y: ");
+        int stop = Convert.ToInt32(Console.ReadLine());
         Console.WriteLine("***************************************************************************");
         Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
         Console.WriteLine("***************************************************************************");
-
-        Console.WriteLine(" Сумма ряда = " + ds.Calculate(startValue, stopValue));
-
+        Console.WriteLine(ds.Calculate(start, stop));
         Console.ReadKey();
     }
 }
