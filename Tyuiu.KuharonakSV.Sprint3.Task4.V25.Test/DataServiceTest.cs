1cbbcc4b2e09cb2a6a1c99ac628d9c6cdded8738
СЕГА <skuhronak@gmail.com>
2025-11-23 21:54:10 +0300

    Task4

--- Tyuiu.KuharonakSV.Sprint3.Task4.V25.Test/DataServiceTest.cs
+++ Tyuiu.KuharonakSV.Sprint3.Task4.V25.Test/DataServiceTest.cs
@@ -1,14 +1,17 @@
 ﻿using Tyuiu.KuharonakSV.Sprint3.Task4.V25.Lib;
 namespace Tyuiu.KuharonakSV.Sprint3.Task4.V25.Test
+public class Tests
 {
-    [TestClass]
-    public sealed class DataServiceTest
+    [SetUp]
+    public void Setup()
     {
-        [TestMethod]
-        public void ValidCalculate()
-        {
-            DataService ds = new DataService();
-            Assert.That(ds.Calculate(-5, -1), Is.EqualTo(8.995));
-        }
     }
+
+    [Test]
+    public void Test1()
+    {
+        DataService ds = new DataService();
+        Assert.That(ds.Calculate(-5, -1), Is.EqualTo(8.995));
+    }
+}
 }
